a8784ec1cfe5622b25fee86186a9d9ffc4e0dd04
Magnus Giverin <magnusgiverin@icloud.com>
2024-02-12 13:37:16 +0100

    Feature: Add component for movie view in personal page

---
+++ frontend/src/app/profile/Collection.tsx
@@ -0,0 +1,35 @@
+import Icons from "@/components/General/Icons";
+import Link from "next/link";
+
+interface CollectionProps {
+    title: string;
+    link: string;
+}
+
+const Rectangle: React.FC = () => {
+    return <div className="w-[140px] h-[210px] bg-white m-2"></div>;
+};
+
+const Collection: React.FC<CollectionProps> = ({ title, link }) => {
+    return (
+        <div className="flex flex flex-col items-center">
+            <div className="flex justify-between w-full">
+                <h1 className="text-xl underline">{title}</h1>
+                <Link href={link}>
+                <h1 className="text-sm flex items-center">
+                    Show more
+                    <Icons name="RightArrow"/>
+                </h1>
+                </Link>
+            </div>
+            <div className="flex">
+                <Rectangle />
+                <Rectangle />
+                <Rectangle />
+                <Rectangle />
+            </div>
+        </div>
+    );
+};
+
+export default Collection;
--- frontend/src/components/General/Icons.tsx
+++ frontend/src/components/General/Icons.tsx
@@ -24,7 +24,11 @@ const Icons: React.FC<IconsProps> = ({ name }) => {
                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth={1.5} className="w-6 h-6 ml-4">
                 <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" strokeLinecap="round" strokeLinejoin="round" />
                 <circle cx="12" cy="7" r="4"></circle>
-            </svg>
+            </svg>,
+        RightArrow:
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+        </svg>
     };
 
     return (
