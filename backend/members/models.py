6ac726780e2c7005518ef9cfa354c5c39355f656
Torgeir Lysen <32740390+torgeirlysen@users.noreply.github.com>
2024-02-15 10:49:27 +0100

    fix(backend, members): change CustomUser id to uuid

--- backend/members/models.py
+++ backend/members/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+import uuid
+
 class CustomUser(AbstractUser):
-    pass
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
