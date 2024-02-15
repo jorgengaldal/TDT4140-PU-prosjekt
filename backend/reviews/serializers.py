015c89968a0c45b5231798cd9a721a420a091472
Torgeir Lysen <32740390+torgeirlysen@users.noreply.github.com>
2024-02-15 10:51:37 +0100

    feat(backend, reviews): make movie a SerializersMethod field in MovieReviewSerializer to display movie information

--- backend/reviews/serializers.py
+++ backend/reviews/serializers.py
@@ -1,18 +1,27 @@
 from typing import List
 from rest_framework import serializers
 from .models import MovieList, MovieReview
+from movies.models import Movie
+from movies.serializers import MovieSerializer
 
 from django_typomatic import ts_interface, generate_ts
 
 
-@ts_interface
+#@ts_interface
 class MovieReviewSerializer(serializers.ModelSerializer):
+    movie = serializers.SerializerMethodField()
+
     class Meta:
         model = MovieReview
         fields = '__all__'
+    
+    def get_movie(self, obj) -> Movie:
+        related_movie = obj.movie
+        serializer = MovieSerializer(related_movie)
+        return serializer.data
 
 
-@ts_interface
+#@ts_interface
 class MovieListSerializer(serializers.ModelSerializer):
     reviews = serializers.SerializerMethodField()
 
