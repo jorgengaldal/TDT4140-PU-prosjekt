965cf66ae5c8132096ec2047492db8ce18c6a2f0
Torgeir Lysen <32740390+torgeirlysen@users.noreply.github.com>
2024-02-16 18:32:42 +0100

    feat(backend, members): create profile when user is created through register post request

--- backend/members/views.py
+++ backend/members/views.py
@@ -4,12 +4,17 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 from .serializers import UserSerializer
+from django.db import transaction
+
+from profiles.models import Profile
 
 class CreateUserView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            with transaction.atomic():
+                user = serializer.save()  
+                Profile.objects.create(user=user)  
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
