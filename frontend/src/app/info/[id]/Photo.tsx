9bdb50ff78c5db663e460a07e04e843c2e5e6f0d
elisabethsanne <elisabeth_sanne@hotmail.com>
2024-02-19 12:56:19 +0100

    Fix: added API fetch

--- frontend/src/app/info/[id]/Photo.tsx
+++ frontend/src/app/info/[id]/Photo.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 interface PhotoProps {
     width: string;
     height: string;
+    imageUrl: string;
 }
 
-const Photo: React.FC<PhotoProps> = ({ width, height }) => {
+const Photo: React.FC<PhotoProps> = ({ width, height, imageUrl }) => {
     return (
         <div className={`bg-orange-500 rounded-lg h-[${height}px] w-[${width}px]`}>
-            <p className="h-[75%]">Insert photo here</p>
+            <img src={imageUrl} alt="Movie Photo" className="w-full h-full object-cover" />
         </div>
     );
 };
-
 export default Photo;
