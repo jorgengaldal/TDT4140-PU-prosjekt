f85294e9c2478d4377979e1f1f38e03f21e21498
Lara Jacobsen <lrjacobs@stud.ntnu.no>
2024-02-19 11:00:10 +0100

    feat: country picker implemented

--- frontend/src/app/profile/Views.tsx
+++ frontend/src/app/profile/Views.tsx
@@ -1,7 +1,33 @@
+import React, { useState, useMemo, useEffect } from 'react'
+import Select from "react-select";
 import Collection from "./Collection";
 import Genres from "./Genres";
 
 const tempGenres = ["Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Romance", "Thriller", "Western"];
+
+const CountrySelect = () => {
+    const [countries, setCountries] = useState([]);
+    const [selectedCountry, setSelectedCountry] = useState({});
+  
+    useEffect(() => {
+      fetch(
+        "https://valid.layercode.workers.dev/list/countries?format=select&flags=true&value=code"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setCountries(data.countries);
+          setSelectedCountry(data.userSelectValue);
+        });
+    }, []);
+    return (
+      <Select
+        options={countries}
+        value={selectedCountry}
+        onChange={(selectedOption) => setSelectedCountry(selectedOption)}
+      />
+    );
+  };
+
 
 export const ViewOne = () => {
     return (
@@ -14,20 +40,58 @@ export const ViewOne = () => {
     );
 }
 
+export const ViewTwo = () => {
+    return (
+        <div className="w-2/3 bg-accent1 h-100% rounded-lg px-24 py-12">
+            <h1 className="text-xl">Preferences</h1>
+            <div className="App">
+            <CountrySelect />
+            </div>
+           
+            <div className="flex flex-col">
+
+
+                <div className="flex justify-between">
+                </div>
+                <div className="flex justify-between">
+                    <h1 className="text-xl underline">Country</h1>
+                    <h1 className="text-xl underline">United States</h1>
+                </div>
+                <div className="flex justify-between">
+                    <h1 className="text-xl underline">Time Zone</h1>
+                    <h1 className="text-xl underline">UTC-07:00</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
 interface DisplayViewProps {
     currentSetting: string;
 }
 
 const DisplayView: React.FC<DisplayViewProps> = ({ currentSetting }) => {
-    if(currentSetting === "main") {
+    if (currentSetting === "main") {
         return (
             <ViewOne />
         )
-    } else {
+    } else if (currentSetting === "Setting2") {
+        return (
+            <ViewTwo />
+        )
+
+    } else if (currentSetting === "Setting3") {
         return (
             <div className="w-2/3 bg-accent1 h-100% rounded-lg px-24 py-12">
-                    <h1>{currentSetting}</h1>
-                </div>
+                <h1>{currentSetting}</h1>
+            </div>
+        )
+
+    } else if (currentSetting === "Setting4") {
+        return (
+            <div className="w-2/3 bg-accent1 h-100% rounded-lg px-24 py-12">
+                <h1>{currentSetting}</h1>
+            </div>
         )
     }
 }
