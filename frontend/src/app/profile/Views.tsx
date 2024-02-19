fad55944766150afd2725bf2413dd8e938146284
Lara Jacobsen <lrjacobs@stud.ntnu.no>
2024-02-19 12:54:22 +0100

    feat: made fields for changing username, password, name and email

--- frontend/src/app/profile/Views.tsx
+++ frontend/src/app/profile/Views.tsx
@@ -1,42 +1,69 @@
-import React from "react";
-import CountrySelect from "./CountrySelect";
-import Collection from "./Collection";
-import Genres from "./Genres";
+import React, { useState } from "react";
 
-const tempGenres = ["Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Romance", "Thriller", "Western"];
+const ViewThree = () => {
+    const [username, setUsername] = useState("");
+    const [name, setName] = useState("");   
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
+    const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value);
+    };
 
-export const ViewOne = () => {
+    const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    };
+
+    const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    };
+
     return (
         <div className="w-2/3 bg-accent1 h-100% rounded-lg px-24 py-12">
-            <Collection title={"My Liked"} link={"liked"} />
-            <Collection title={"Watched"} link={"watched"} />
-            <Genres title={"Your Top Genres"} genres={tempGenres} />
-            <Collection title={"Watch List"} link={"watchlist"} />
-        </div>
-    );
-}
-
-export const ViewTwo = () => {
-    return (
-        <div className="w-2/3 bg-accent1 h-100% rounded-lg px-24 py-12">
-            <h1 className="text-xl">Preferences</h1>
-            <div className="App">
-            <CountrySelect />
-            </div>
-           
+            <h1 className="text-xl underline">Account Settings</h1>
             <div className="flex flex-col">
-
-
-                <div className="flex justify-between">
+                <div className="flex justify-between border-b border-[#7077A1] py-7">
+                    <h1 className="text-xl">Username</h1>
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={handleUsernameChange}
+                        className="text-xl"
+                    />
                 </div>
-                <div className="flex justify-between">
-                    <h1 className="text-xl underline">Country</h1>
-                    <h1 className="text-xl underline">United States</h1>
+
+                <div className="flex justify-between border-b border-[#7077A1] py-7">
+                    <h1 className="text-xl">Name</h1>
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={handleNameChange}
+                        className="text-xl"
+                    />
                 </div>
-                <div className="flex justify-between">
-                    <h1 className="text-xl underline">Time Zone</h1>
-                    <h1 className="text-xl underline">UTC-07:00</h1>
+
+                <div className="flex justify-between border-b border-[#7077A1] py-7">
+                    <h1 className="text-xl">Email</h1>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={handleEmailChange}
+                        className="text-xl"
+                    />
+                </div>
+
+                <div className="flex justify-between border-b border-[#7077A1] py-7">
+                    <h1 className="text-xl">Password</h1>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                        className="text-xl"
+                    />
                 </div>
             </div>
         </div>
@@ -56,14 +83,10 @@ const DisplayView: React.FC<DisplayViewProps> = ({ currentSetting }) => {
         return (
             <ViewTwo />
         )
-
     } else if (currentSetting === "Setting3") {
         return (
-            <div className="w-2/3 bg-accent1 h-100% rounded-lg px-24 py-12">
-                <h1>{currentSetting}</h1>
-            </div>
+            <ViewThree />
         )
-
     } else if (currentSetting === "Setting4") {
         return (
             <div className="w-2/3 bg-accent1 h-100% rounded-lg px-24 py-12">
@@ -73,4 +96,4 @@ const DisplayView: React.FC<DisplayViewProps> = ({ currentSetting }) => {
     }
 }
 
-export default DisplayView
+export default DisplayView;
