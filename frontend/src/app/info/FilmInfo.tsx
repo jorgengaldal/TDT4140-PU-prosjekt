325cb15195891db8aefbbb8a2897911049a540c2
Olea Gjeldvik <oleaig@stud.ntnu.no>
2024-02-20 16:00:25 +0100

    Feat: added interface

--- frontend/src/app/info/FilmInfo.tsx
+++ frontend/src/app/info/FilmInfo.tsx
@@ -7,6 +7,13 @@ interface FilmInfoProps {
     selectedMovieId: string;
 }
 
+interface Review{
+    review_text: string;
+    rating: number;
+    is_favorite: boolean;
+    movie_list: string;
+}
+
 interface Movie {
     imdbid: string;
     title: string;
@@ -22,15 +29,17 @@ interface Movie {
 export default function FilmInfo({ selectedMovieId }: FilmInfoProps) {
     const [movieData, setMovieData] = useState<Movie | null>(null);
 
-    const [isClicked1, setIsClicked1] = useState<boolean>(false);
-    const [isClicked2, setIsClicked2] = useState<boolean>(false);
+    const [isClickedWatched, setIsClickedWatched] = useState<boolean>(false);
+    const [isClickedHeart, setIsClickedHeart] = useState<boolean>(false);
 
-    const handleClick1 = () => {
-        setIsClicked1(!isClicked1);
+    const handleClickWatched = () => {
+        const review = 
+        fetch ('http://localhost:8000/api/reviews/moviereviews')
+        setIsClickedWatched(!isClickedWatched);
     };
 
-    const handleClick2 = () => {
-        setIsClicked2(!isClicked2);
+    const handleClickHeart = () => {
+        setIsClickedHeart(!isClickedHeart);
     };
 
     useEffect(() => {
@@ -66,8 +75,8 @@ export default function FilmInfo({ selectedMovieId }: FilmInfoProps) {
                 <div className="p-4 w-4/5">
                     <div className="flex flex-row">
                         <h1 className="text-2xl mr-4">{movieData && movieData.title}</h1>
-                        <button onClick={handleClick1}>
-                            {isClicked1 ? (
+                        <button onClick={handleClickWatched}>
+                            {isClickedWatched ? (
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                     <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
                                     <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z" clipRule="evenodd" />
@@ -79,8 +88,8 @@ export default function FilmInfo({ selectedMovieId }: FilmInfoProps) {
                             )}
                         </button>
                         <h1 className="text-2xl mr-4">{/* content here */}</h1> {/* Add your content here */}
-                        <button onClick={handleClick2}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill={isClicked2 ? "white" : "none"} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <button onClick={handleClickHeart}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill={isClickedHeart ? "white" : "none"} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                             </svg>
                         </button>
