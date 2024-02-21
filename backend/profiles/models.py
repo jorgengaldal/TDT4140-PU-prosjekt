669355fc67c99a45ca45660782cbb5976e241790
Torgeir Lysen <32740390+torgeirlysen@users.noreply.github.com>
2024-02-21 15:47:29 +0100

    feat(backend/profiles): add a my_movie_list field with the primary key of the default movielist owned by the profile on creation

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from members.models import CustomUser
+from django.apps import apps
 
 import uuid
 
@@ -8,6 +9,7 @@ class Profile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    my_movie_list = models.ForeignKey("reviews.MovieList", on_delete=models.SET_NULL, null=True, blank=True, related_name='profile_owner')
 
     def __str__(self):
         return self.user.username
