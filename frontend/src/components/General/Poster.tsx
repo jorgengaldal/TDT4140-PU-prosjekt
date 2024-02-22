f2799bbac1edf88083e6838086f3cb10a3f9af6a
Lara Jacobsen <lrjacobs@stud.ntnu.no>
2024-02-22 10:39:02 +0100

    style: finalise style for view of movies

--- frontend/src/components/General/Poster.tsx
+++ frontend/src/components/General/Poster.tsx
@@ -7,23 +7,26 @@ export default function Poster(movie: any, index: number) {
     window.location.href = "/info?id=" + movie.imdbid;
   }
 
-  movie = movie.movie;
-  index = movie.index;
+  movie = movie.movie
+  index = movie.index
 
   return (
-    <div className="posterComponent">
+    <div className="pt-5 flex flex-col items-center">
+
       <img
-        className="posterImage"
+        className="text-2xl mt-5 mb-5 text-center text-white hover:transform hover:scale-110 transition duration-300 cursor-pointer"
         key={index}
         onClick={handleClick}
-        src={movie.poster || "/testImg1.jpg"}
+        src={movie.poster || '/testImg1.jpg'}
         alt={`Movie Poster ${index}`}
         onError={(e) => {
           (e.target as HTMLImageElement).onerror = null;
-          (e.target as HTMLImageElement).src = "/testImg1.jpg";
+          (e.target as HTMLImageElement).src = '/testImg1.jpg';
         }}
+        style={{ width: "250px", height: "320px" }} // Add this line
       />
-      <p className="text-lg mt-2 mb-2 text-center text-white shadow-lg">{movie.title}</p>
+      <p className="text-lg mt-2 mb-2 text-center text-white">{movie.title}</p>
+
     </div>
   );
 }
