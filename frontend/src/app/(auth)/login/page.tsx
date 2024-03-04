5cad6d573b9b2b26a78b34ca65ca18c7b93b8a05
Torgeir Keun Lysen <torgeir.k.lysen@ntnu.no>
2024-03-04 13:48:31 +0100

    Resolve "Innloggingside (HØY PRIORITET!)"

---
+++ frontend/src/app/(auth)/login/page.tsx
@@ -0,0 +1,143 @@
+"use client";
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Cookie from "js-cookie";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { useRouter } from "next/navigation";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Card, CardContent, CssBaseline } from "@mui/material";
+
+const defaultTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+export default function SignIn() {
+  const router = useRouter();
+  const [error, setError] = React.useState("");
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("username"),
+      password: data.get("password"),
+    });
+    fetch("http://localhost:8000/api/members/login/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: data.get("username"),
+        password: data.get("password"),
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.token) {
+          Cookie.set("token", data.token, { expires: 52 * 7 });
+          console.log("Token stored successfully");
+          router.push("/");
+        } else {
+          console.log("No token in response");
+          setError(data["error"]);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    <div className="min-h-screen m-20">
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <Card sx={{ bgcolor: "rgba(255, 255, 255, 0.005)" }}>
+            <CardContent>
+              <Box
+                sx={{
+                  marginTop: 4,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Sign in
+                </Typography>
+                <Typography color="error">{error}</Typography>
+                <Box
+                  component="form"
+                  onSubmit={handleSubmit}
+                  noValidate
+                  sx={{ mt: 1 }}
+                >
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="username"
+                    label="Username"
+                    name="username"
+                    autoComplete="username"
+                    autoFocus
+                  />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+                  {/* <FormControlLabel
+                    control={<Checkbox value="remember" color="primary" />}
+                    label="Remember me"
+            />*/}
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    style={{
+                      backgroundColor: "#45ACFF",
+                      color: "white",
+                    }}
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Sign In
+                  </Button>
+                  <Grid container>
+                    {/*<Grid item xs>
+                      <Link href="#" variant="body2">
+                        Forgot password?
+                      </Link>
+        </Grid>*/}
+                    <Grid item>
+                      <Link href="/signup" variant="body2">
+                        {"Don't have an account? Sign Up"}
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Box>
+            </CardContent>
+          </Card>
+        </Container>
+      </ThemeProvider>
+    </div>
+  );
+}
