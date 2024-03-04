a783238b96aab1ca19d40042eaa1ee338e884e62
Torgeir Lysen <32740390+torgeirlysen@users.noreply.github.com>
2024-03-04 13:00:28 +0100

    fix: redesign filminfo component of info page

--- frontend/src/app/(dashboard)/info/FilmInfo.tsx
+++ frontend/src/app/(dashboard)/info/FilmInfo.tsx
@@ -1,9 +1,20 @@
 "use client";
-
 import Link from "next/link";
-import Photo from "./Photo";
 import { useState, useEffect } from "react";
-import Cookie from 'js-cookie';
+import Cookie from "js-cookie";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import StarRoundedIcon from "@mui/icons-material/StarRounded";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import {
+  Box,
+  Card,
+  CardMedia,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
 
 interface FilmInfoProps {
   selectedMovieId: string;
@@ -25,6 +36,7 @@ interface Movie {
   genres: string[];
   actors: string[];
   directors: string[];
+  writers: string[];
   released: string;
   imdbrating: string;
 }
@@ -35,7 +47,7 @@ export default function FilmInfo({ selectedMovieId }: FilmInfoProps) {
   const [isClickedWatched, setIsClickedWatched] = useState<boolean>(false);
   const [isClickedHeart, setIsClickedHeart] = useState<boolean>(false);
 
-  const authToken = Cookie.get('token');
+  const authToken = Cookie.get("token");
 
   const authHeaders = {
     "Content-Type": "application/json",
@@ -51,9 +63,8 @@ export default function FilmInfo({ selectedMovieId }: FilmInfoProps) {
     });
 
   const handleClickWatched = () => {
-    if (isClickedWatched) {
-      return;
-    }
+    setIsClickedWatched(!isClickedWatched);
+    return;
     if (movieData == null) {
       console.error("There's no movie to mark as watched.");
       return;
@@ -77,7 +88,7 @@ export default function FilmInfo({ selectedMovieId }: FilmInfoProps) {
     });
 
     // TODO: Kan bare markere som sett, ikke som usett.
-    setIsClickedWatched(true);
+    setIsClickedWatched(!isClickedWatched);
   };
 
   const handleClickHeart = () => {
@@ -126,111 +137,131 @@ export default function FilmInfo({ selectedMovieId }: FilmInfoProps) {
   }, [selectedMovieId]);
 
   return (
-    <div className="w-2/3 mt-10 bg-accent2 rounded-t-lg h-[800px]">
-      <div className="flex flex-row">
-        {movieData && (
-          <div className="p-4 w-1/5 rounded-lg">
-            <Photo width="150" height="200" imageUrl={movieData.poster} />
-          </div>
-        )}
-        <div className="p-4 w-4/5">
-          <div className="flex flex-row">
-            <h1 className="text-2xl mr-4">{movieData && movieData.title}</h1>
-            <button onClick={handleClickWatched}>
-              {isClickedWatched ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-                  <path
-                    fillRule="evenodd"
-                    d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9"
-                  />
-                </svg>
-              )}
-            </button>
-            <h1 className="text-2xl mr-4">{/* content here */}</h1>{" "}
-            {/* Add your content here */}
-            <button onClick={handleClickHeart}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill={isClickedHeart ? "white" : "none"}
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
+    <Box
+      className="w-2/3 mt-10 rounded-t-lg"
+      sx={{ backgroundColor: "#262B47", boxShadow: 4 }}
+    >
+      <Grid container spacing={4} sx={{ padding: 3, marginBottom: 5 }}>
+        <Grid item md={3}>
+          <div>
+            {movieData && (
+              <Card
+                sx={{
+                  borderRadius: "8px",
+                  bgcolor: "transparent",
+                  width: "250px",
+                  aspectRatio: "17/25",
+                }}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-                />
-              </svg>
-            </button>
+                <CardMedia
+                  sx={{
+                    height: "100%",
+                    width: "100%",
+                    "& img": {
+                      objectFit: "fill",
+                      width: "100%",
+                      height: "100%",
+                    },
+                  }}
+                >
+                  <img src={movieData.poster} alt="Movie Photo" />
+                </CardMedia>
+              </Card>
+            )}
           </div>
-          <div className="">
-            <h1 className="">{movieData && movieData.released}</h1>
-            <h1 className="text-1g mt-2 mx-10">
-              {movieData &&
-                movieData.genres.map((genre, index) => (
-                  <Link key={index} href={"/category?name=" + genre}>
-                    <span className="mx-2">{genre}</span>
-                  </Link>
-                ))}
-            </h1>
-          </div>
-          <h1 className="mt-3 text-2 mr-4">{movieData && movieData.plot}</h1>
-          <p className="mt-8">Imdb rating:</p>{" "}
-          <p>{movieData && movieData.imdbrating}</p>
-          {/* <p className="mt-10">Add personal rating:</p>
-          <p className="mt-1">Number:</p>
-          <p className="mt-1">Description:</p> */}
-        </div>
-      </div>
+        </Grid>
+        <Grid item container md={9}>
+          <Grid item md={12}>
+            <div className="flex flex-row mb-3">
+              <Typography
+                variant="h3"
+                className="mr-4"
+                sx={{ textShadow: "1px 1px 2px black" }}
+              >
+                {movieData && movieData.title}
+              </Typography>
+              <IconButton
+                className="mr-2"
+                sx={{ color: "white" }}
+                onClick={handleClickWatched}
+              >
+                {isClickedWatched ? (
+                  <VisibilityIcon sx={{ fontSize: 35 }} />
+                ) : (
+                  <VisibilityOffIcon sx={{ fontSize: 35 }} />
+                )}
+              </IconButton>
+              <IconButton sx={{ color: "pink" }} onClick={handleClickHeart}>
+                {isClickedHeart ? (
+                  <FavoriteIcon sx={{ fontSize: 35 }} />
+                ) : (
+                  <FavoriteBorderIcon sx={{ fontSize: 35 }} />
+                )}
+              </IconButton>
+            </div>
+            <div className="flex flex-row mb-6">
+              <p>{movieData && movieData.released} | </p>
+              <p className="text-1g ml-2.5" style={{ color: "LightCyan" }}>
+                {movieData &&
+                  movieData.genres.map((genre, index) => (
+                    <Link key={index} href={"/category?name=" + genre}>
+                      <span className="mx-2">{genre} </span>
+                    </Link>
+                  ))}
+              </p>
+            </div>
+            <h1 className="mt-3 text-2 mr-4">{movieData && movieData.plot}</h1>
+            <div className="mt-5">
+              Directors:
+              <span className="text-1g ml-2.5" style={{ color: "LightCyan" }}>
+                {movieData?.directors.join(", ")}
+              </span>
+              <div>
+                Writers:
+                <span className="text-1g ml-2.5" style={{ color: "LightCyan" }}>
+                  {movieData?.writers.join(", ")}
+                </span>
+              </div>
+            </div>
+            <a href={`https://www.imdb.com/title/${movieData?.imdbid}/`}>
+              <div className="mt-10">
+                <Typography sx={{ fontSize: 15, opacity: 0.8 }}>
+                  IMDb RATING:
+                </Typography>
+                <div>
+                  <Typography sx={{ fontSize: 20 }}>
+                    <StarRoundedIcon sx={{ color: "#F5C519", fontSize: 30 }} />
 
-      <div className="w-full relative inset-x-0 bottom-0 bg-accent1 rounded-lg h-[55%]">
-        <p className="mt-40">Top 3 cast:</p>
-        <div className="p-4">
-          <h1 className="text-1g mt-2 mx-2">
-            {movieData &&
-              movieData.actors.map((actor, index) => (
-                <Link key={index} href={"/persons?name=" + actor}>
-                  <p className="mx-2">{actor}</p>
-                </Link>
-              ))}
-          </h1>
-        </div>
-        <p className="mt-10">Directors:</p>
-        <div className="p-4">
-          <h1 className="text-1g mt-2 mx-2">
-            {movieData &&
-              movieData.directors.map((director, index) => (
-                <Link key={index} href={"/persons?name=" + director}>
-                  <p className="mx-2">{director}</p>
-                </Link>
-              ))}
-          </h1>
+                    {movieData && movieData.imdbrating}
+                    <span style={{ opacity: 0.8, fontSize: 17 }}>/10</span>
+                  </Typography>
+                </div>
+              </div>
+            </a>
+          </Grid>
+        </Grid>
+      </Grid>
+      <div className="w-full relative inset-x-0 bottom-0 h-[55%] bg-[#202439]">
+        <div className="p-10">
+          <Typography variant="h5" className="mb-4">
+            Top Cast:
+          </Typography>
+          <div className="flex flex-row mb-3 space-x-4">
+            {movieData?.actors.map((actor, index) => (
+              <div>
+                <div className="rounded-lg bg-transparent w-[150px] h-[calc(150px*25/17)] overflow-hidden mb-3">
+                  <img
+                    src="https://m.media-amazon.com/images/M/MV5BMjExNzA4MDYxN15BMl5BanBnXkFtZTcwOTI1MDAxOQ@@._V1_.jpg"
+                    alt="Movie Photo"
+                    className="object-fill w-full h-full"
+                  />
+                </div>
+                <Typography>{actor}</Typography>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </Box>
   );
 }
