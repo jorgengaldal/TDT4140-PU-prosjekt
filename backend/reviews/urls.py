cfb044950cf139b549b3d103117b34aa284a32cc
Gaute Greiff Flagstad <gautegf@stud.ntnu.no>
2024-03-14 11:59:08 +0100

    feat: added path to detailed view of liked not movie

--- backend/reviews/urls.py
+++ backend/reviews/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('movielist/', views.MovieListListView.as_view(), name='movie_list_list'),
     path('movielist/<uuid:pk>/', views.MovieListDetailView.as_view(), name='movie_list_detail'),
     path('likednotmovies/', views.LikedNotMovieListView.as_view(), name='liked_not_movie_list'),
+    path('likednotmovies/<uuid:pk>/', views.LikedNotMovieDetailView.as_view(), name='likednotmovie-delete')
 ]
