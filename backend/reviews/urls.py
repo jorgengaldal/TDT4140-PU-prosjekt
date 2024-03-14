f0c8cd12cdf464031b6284abed768704e9867ac8
Gaute Greiff Flagstad <gautegf@stud.ntnu.no>
2024-03-14 09:02:11 +0100

    feat: added path for likedNotMovies

--- backend/reviews/urls.py
+++ backend/reviews/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('moviereviews/<uuid:pk>/', views.MovieReviewDetailView.as_view(), name='movie_review_detail'),
     path('movielist/', views.MovieListListView.as_view(), name='movie_list_list'),
     path('movielist/<uuid:pk>/', views.MovieListDetailView.as_view(), name='movie_list_detail'),
+    path('likednotmovies/', views.LikedNotMovieListView.as_view(), name='liked_not_movie_list'),
 ]
